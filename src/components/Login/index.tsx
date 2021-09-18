98cf02884d551fe3f794f2c1bcda6cd7657ae56b
Jesper Johansson <jesper.k.johansson@gmail.com>
2021-09-18 17:21:52 +0200

    Updated login page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import urls from "../../utils/urls";
 import { Box, Button, Typography } from "@material-ui/core";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { ITheme } from "../../lib/types";
 
+
+const useStyles = makeStyles((theme: ITheme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "60vh",
+    },
+    title: {
+      fontWeight: "bold",
+      color: theme.palette.themePrimary.darkTextColor,
+    },
+    button: {
+      marginTop: 64,
+      color: theme.palette.themePrimary.darkTextColor,
+      backgroundColor: theme.palette.themeSecondary.backgroundColor,
+      "&:hover": {
+        backgroundColor: theme.palette.themePrimary.backgroundColor,
+      },
+    },
+  }),
+);
 
 const Login = () => {
+  const classes = useStyles();
+
   const clientId = "08229ccf0cc24b99b8d855af166bec00";
   const redirectUri = urls.redirect;
   const authEndpoint = urls.authEndpoint;
@@ -11,10 +38,11 @@ const Login = () => {
   const authUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
 
   return (
+    <div className={classes.root}>
       <Box display="flex" flexDirection="column" alignItems="center">
         <Typography
           variant="h2"
-          style={{ textAlign: "center", fontWeight: "bold" }}
+          className={classes.title}
         >
           Spotify Statistics
         </Typography>
@@ -22,10 +50,12 @@ const Login = () => {
           size="large"
           variant="outlined"
           href={authUrl}
+          className={classes.button}
         >
           Login with Spotify
         </Button>
       </Box>
+    </div>
   );
 };
 
